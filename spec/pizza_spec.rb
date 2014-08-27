0312fb50341457c1f072a9f26d2c2314c53f3492
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-08-27 16:06:14 -0500

    Allows Topping to initialize with name.

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -7,14 +7,14 @@ describe Pizza do
 end
 
 describe Topping do
-  it "exists" do
-    expect(Topping).to be_a(Class)
+  # it "exists" do
+  #   expect(Topping).to be_a(Class)
+  #   end
   describe '.initialize' do
     it "sets the name of the topping" do
       topping = Topping.new('olives')
 
       expect(topping.name).to eq('olives')
-    end
   end
 end
 end
