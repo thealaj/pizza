0f9cf6add11c9fe0d9a2f3ff2b55487b37cb88ee
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-08-27 17:05:30 -0500

    Set up a test of the default parameter in the Pizza class

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -11,10 +11,17 @@ describe Pizza do
         Topping.new('pepperoni')
       ]
       pizza = Pizza.new(toppings)
-      expect(pizza.toppings).to eq(toppings)
+
+      expect(pizza.toppings).to eq(Array)
+    end
+    it 'defaults the toppings to cheese only if the pizza has no toppings' do
+      pizza = Pizza.new
+      expect(pizza.toppings.size).to eq(1)
+      expect(pizza.toppings.first.name).to eq('cheese')
     end
   end
 end
+
 
 describe Topping do
   it "exists" do
@@ -25,15 +32,15 @@ describe Topping do
       topping = Topping.new('olives')
 
       expect(topping.name).to eq('olives')
+    end
   end
-end
   describe '.initialize' do
     it "sets the name of the topping" do
       topping = Topping.new('bell peppers', vegetarian: true)
 
       expect(topping.vegetarian).to eq(true)
+    end
   end
-end
 
 
 end
